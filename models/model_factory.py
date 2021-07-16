75ca6084e73975f069bcc2a535e85a2444da1f99
JoaoGranja <joaogranja10@hotmail.com>
2021-07-16 14:40:51 +0100

    Added more CNN models

--- models/model_factory.py
+++ models/model_factory.py
@@ -1,8 +1,20 @@
-from models.models import resnet152_fpn, resnet101_fpn, resnet50_fpn, xception_fpn,  densenet_fpn, LeNet
+from models.models import resnet152, resnet101, resnet50, xception,  densenet, LeNet
 
 
 def make_model(network, input_shape, num_classes):
     if network == 'LeNet':
         return LeNet(input_shape, num_classes, activation="softmax")
+    elif network == 'resnet152':
+        return resnet152(input_shape, num_classes, activation="softmax")
+    elif network == 'resnet101':
+        return resnet101(input_shape, num_classes, activation="softmax")
+    elif network == 'resnet50':
+        return resnet50(input_shape, num_classes, activation="softmax")
+    elif network == 'resnet18':
+        return resnet50(input_shape, num_classes, activation="softmax")
+    elif network == 'densenet169':
+        return densenet(input_shape, num_classes, activation="softmax")
+    elif network == 'xception':
+        return xception(input_shape, num_classes, activation="softmax")
     else:
         raise ValueError('unknown network ' + network)
