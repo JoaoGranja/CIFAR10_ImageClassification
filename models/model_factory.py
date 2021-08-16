f92b0ecfeb7f8afc0843b1b48040e59e4e46e5bd
JoaoGranja <joaogranja10@hotmail.com>
2021-08-16 21:25:12 +0100

    Update notebook file and remove unused files

--- models/model_factory.py
+++ models/model_factory.py
@@ -1,20 +1,20 @@
 from models.models import resnet101, resnet50, inception, vgg19, vgg16, efficientNet, leNet
 
 
-def make_model(network, input_shape, num_classes):
+def make_model(network, input_shape, num_classes, fine_tuning=False):
     if network == 'LeNet':
-        return leNet(input_shape, num_classes, activation="softmax")
+        return leNet(input_shape, num_classes, fine_tuning, activation="softmax")
     elif network == 'VGG19':
-        return vgg19(input_shape, num_classes, activation="softmax")
+        return vgg19(input_shape, num_classes, fine_tuning, activation="softmax")
     elif network == 'VGG16':
-        return vgg16(input_shape, num_classes, activation="softmax")
+        return vgg16(input_shape, num_classes, fine_tuning, activation="softmax")
     elif network == 'resnet50':
-        return resnet50(input_shape, num_classes, activation="softmax")
+        return resnet50(input_shape, num_classes, fine_tuning, activation="softmax")
     elif network == 'resnet101':
-        return resnet101(input_shape, num_classes, activation="softmax")
+        return resnet101(input_shape, num_classes, fine_tuning, activation="softmax")
     elif network == 'efficientNet':
-        return efficientNet(input_shape, num_classes, activation="softmax")
+        return efficientNet(input_shape, num_classes, fine_tuning, activation="softmax")
     elif network == 'inception':
-        return inception(input_shape, num_classes, activation="softmax")
+        return inception(input_shape, num_classes, fine_tuning, activation="softmax")
     else:
         raise ValueError('unknown network ' + network)
--- models/models.py
+++ models/models.py
@@ -1,11 +1,11 @@
 
 from tensorflow.keras import Model, Input, Sequential
-from tensorflow.keras.layers import Dense, MaxPooling2D, Flatten, Conv2D, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense, MaxPooling2D, Flatten, Conv2D, GlobalAveragePooling2D, Dropout, BatchNormalization
 from tensorflow.keras.utils import get_file
 from tensorflow.keras.applications import VGG16, VGG19, ResNet50, ResNet101, EfficientNetB0, InceptionV3
 
 
-def vgg16(input_shape, num_classes, activation="softmax"):
+def vgg16(input_shape, num_classes, fine_tuning=False, activation="softmax"):
     img_input = Input(shape=input_shape)
     base_model = VGG16(
                     include_top=False,
@@ -13,16 +13,20 @@ def vgg16(input_shape, num_classes, activation="softmax"):
                     input_shape=input_shape, 
     )  # Do not include the ImageNet classifier at the top.
     # Freeze the base_model
-    base_model.trainable = True
+    base_model.trainable = fine_tuning
     
     #Build the model
     x = base_model(img_input, training=False)
     x = GlobalAveragePooling2D()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.3)(x)
     outputs = Dense(num_classes, activation=activation)(x) #Adds a fully connected layer with output = num_classes
     model = Model(img_input, outputs, name="VGG16")
     return model
 
-def vgg19(input_shape, num_classes, activation="softmax"):
+def vgg19(input_shape, num_classes, fine_tuning=False, activation="softmax"):
     img_input = Input(shape=input_shape)
     base_model = VGG19(
                     include_top=False,
@@ -30,17 +34,20 @@ def vgg19(input_shape, num_classes, activation="softmax"):
                     input_shape=input_shape, 
     )  # Do not include the ImageNet classifier at the top.
     # Freeze the base_model
-    base_model.trainable = True
+    base_model.trainable = fine_tuning
     
     #Build the model
-    model=Sequential(name="VGG19")
-    model.add(img_input)   #Adds the input layer
-    model.add(base_model)  #Adds the base model (in this case vgg19)
-    model.add(Flatten())   #Flatten base model output
-    model.add(Dense(num_classes, activation=activation)) #Adds a fully connected layer with output = num_classes
+    x = base_model(img_input, training=False)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    outputs = Dense(num_classes, activation=activation)(x) #Adds a fully connected layer with output = num_classes
+    model = Model(img_input, outputs, name="VGG19")  
     return model
 
-def resnet50(input_shape, num_classes, activation="softmax"):
+def resnet50(input_shape, num_classes, fine_tuning=False, activation="softmax"):
     img_input = Input(shape=input_shape)
     base_model = ResNet50(
                     include_top=False,
@@ -50,15 +57,20 @@ def resnet50(input_shape, num_classes, activation="softmax"):
     )  # Do not include the ImageNet classifier at the top.
 
     # Freeze the base_model
-    base_model.trainable = False
-    model=Sequential(name="resnet50")
-    model.add(img_input)   #Adds the input layer
-    model.add(base_model)  #Adds the base model (in this case resnet50)
-    model.add(Flatten())   #Flatten base model output
-    model.add(Dense(num_classes, activation=activation)) #Adds a fully connected layer with output = num_classes
+    base_model.trainable = fine_tuning
+    
+    #Build the model
+    x = base_model(img_input, training=False)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    outputs = Dense(num_classes, activation=activation)(x) #Adds a fully connected layer with output = num_classes
+    model = Model(img_input, outputs, name="resnet50")  
     return model
 
-def resnet101(input_shape, num_classes, activation="softmax"):
+def resnet101(input_shape, num_classes, fine_tuning=False, activation="softmax"):
     img_input = Input(shape=input_shape)
     base_model = ResNet101(
                     include_top=False,
@@ -66,17 +78,21 @@ def resnet101(input_shape, num_classes, activation="softmax"):
                     input_shape=input_shape,  
                     classes=10,
     )  # Do not include the ImageNet classifier at the top.
-
     # Freeze the base_model
-    base_model.trainable = False
-    model=Sequential(name="resnet101")
-    model.add(img_input)   #Adds the input layer
-    model.add(base_model)  #Adds the base model (in this case resnet101)
-    model.add(Flatten())   #Flatten base model output
-    model.add(Dense(num_classes, activation=activation)) #Adds a fully connected layer with output = num_classes
+    base_model.trainable = fine_tuning
+    
+    #Build the model
+    x = base_model(img_input, training=False)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    outputs = Dense(num_classes, activation=activation)(x) #Adds a fully connected layer with output = num_classes
+    model = Model(img_input, outputs, name="resnet101")  
     return model
 
-def inception(input_shape, num_classes, activation="sigmoid"):
+def inception(input_shape, num_classes, fine_tuning=False, activation="softmax"):
     img_input = Input(shape=input_shape)
     base_model = InceptionV3(
                     include_top=False,
@@ -84,17 +100,21 @@ def inception(input_shape, num_classes, activation="sigmoid"):
                     input_shape=input_shape,  
                     classes=10,
     )  # Do not include the ImageNet classifier at the top.
-
     # Freeze the base_model
-    base_model.trainable = False
-    model=Sequential(name="inception")
-    model.add(img_input)   #Adds the input layer
-    model.add(base_model)  #Adds the base model (in this case xception)
-    model.add(Flatten())   #Flatten base model output
-    model.add(Dense(num_classes, activation=activation)) #Adds a fully connected layer with output = num_classes
+    base_model.trainable = fine_tuning
+    
+    #Build the model
+    x = base_model(img_input, training=False)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    outputs = Dense(num_classes, activation=activation)(x) #Adds a fully connected layer with output = num_classes
+    model = Model(img_input, outputs, name="inception")  
     return model
 
-def efficientNet(input_shape, num_classes, activation="sigmoid"):
+def efficientNet(input_shape, num_classes, fine_tuning=False, activation="softmax"):
     img_input = Input(shape=input_shape)
     base_model = EfficientNetB0(
                     include_top=False,
@@ -102,45 +122,58 @@ def efficientNet(input_shape, num_classes, activation="sigmoid"):
                     input_shape=input_shape,  
                     classes=10,
     )  # Do not include the ImageNet classifier at the top.
-
     # Freeze the base_model
-    base_model.trainable = False
-    model=Sequential(name="efficientNet")
-    model.add(img_input)   #Adds the input layer
-    model.add(base_model)  #Adds the base model (in this case EfficientNetB0)
-    model.add(Flatten())   #Flatten base model output
-    model.add(Dense(num_classes, activation=activation)) #Adds a fully connected layer with output = num_classes
+    base_model.trainable = fine_tuning
+    
+    #Build the model
+    x = base_model(img_input, training=False)
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    outputs = Dense(num_classes, activation=activation)(x) #Adds a fully connected layer with output = num_classes
+    model = Model(img_input, outputs, name="efficientNet")  
     return model
 
-def leNet(input_shape, num_classes, activation="sigmoid"):   
+def leNet(input_shape, num_classes, fine_tuning=False, activation="softmax"):   
     
     inputs = Input(input_shape)
     c1 = Conv2D(16, (5, 5), activation='relu', padding='same') (inputs)
-    #c1 = Dropout(0.1) (c1)
+    c1 = BatchNormalization()(c1)
+    c1 = Dropout(0.1) (c1)
     c1 = MaxPooling2D((2, 2)) (c1)
 
     c2 = Conv2D(32, (5, 5), activation='relu', padding='same') (c1)
-    #c2 = Dropout(0.1) (c2)
+    c2 = BatchNormalization()(c2)
+    c2 = Dropout(0.1) (c2)
     c2 = MaxPooling2D((2, 2)) (c2)
 
     c3 = Conv2D(64, (3, 3), activation='relu', padding='same') (c2)
-    #c3 = Dropout(0.2) (c3)
+    c3 = BatchNormalization()(c3)
+    c3 = Dropout(0.2) (c3)
     c3 = MaxPooling2D((2, 2)) (c3)
 
     c4 = Conv2D(128, (3, 3), activation='relu', padding='same') (c3)
-    #c4 = Dropout(0.2) (c4)
+    c4 = BatchNormalization()(c4)
+    c4 = Dropout(0.2) (c4)
     c4 = MaxPooling2D(pool_size=(2, 2)) (c4)
 
     c5 = Conv2D(256, (3, 3), activation='relu', padding='same') (c4)
-    #c5 = Dropout(0.3) (c5)
+    c5 = BatchNormalization()(c5)
+    c5 = Dropout(0.3) (c5)
     c5 = MaxPooling2D(pool_size=(2, 2)) (c5)
     
     f = Flatten()(c5)
 
     # Fully Connected 1
     d1 = Dense(1000, activation='relu')(f)
+    d1 = BatchNormalization()(d1)
+    d1 = Dropout(0.3) (d1)
     # Fully Connected 2
     d2 = Dense(100, activation='relu')(d1)
+    d2 = BatchNormalization()(d2)
+    d2 = Dropout(0.3) (d2)
     # Fully Connected 3
     outputs = Dense(num_classes, activation=activation)(d2)
     
